8d78ca4071deac87063fe06778e6ad097340fa2c
Yc <ycchina1997@gmail.com>
2019-10-05 18:38:43 +0800

    1

--- pages/equipment/index.js
+++ pages/equipment/index.js
@@ -1,86 +1,87 @@
 // pages/equipment/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    have: 1, //有无蓝牙连接设备
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        have: 1, //有无蓝牙连接设备
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    },
 
-  },
-  /**
-   * 点击跳转开始测量体温
-   */
-  startMeasuring: function () {
-    wx.navigateTo({
-      url: 'startMeasuring',
-    });
-  },
-  /**
-   * 奶嘴点击开始检测体温
-   */
-  startingTest:function(){
-    console.log("开始检测")
-  },
-  /**
-   * 奶嘴点击跳转历史记录
-   */
-  jumpHistory:function(){
-    console.log("历史记录")
-  }
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    },
+    /**
+     * 点击跳转开始测量体温
+     */
+    startMeasuring: function() {
+        wx.navigateTo({
+            url: 'startMeasuring',
+        });
+    },
+    /**
+     * 奶嘴点击开始检测体温
+     */
+    startingTest: function() {
+        console.log("开始检测")
+    },
+    /**
+     * 奶嘴点击跳转历史记录
+     */
+    jumpHistory: function() {
+        console.log("历史记录")
+    }
 })
